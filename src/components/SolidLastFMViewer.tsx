5908feaaccbe40c426da2a6ae513cedcd6ab40ca
ZOASR <44146518+ZOASR@users.noreply.github.com>
2023-12-26 19:04:31 +0200

    performance: added preconnect links to root component

--- src/components/SolidLastFMViewer.tsx
+++ src/components/SolidLastFMViewer.tsx
@@ -59,134 +59,150 @@ const SolidLastFMViewer = ({ api_key, user, updateInterval }: Props) => {
 		updateInterval
 	});
 	return (
-		<lfmContext.Provider
-			value={{ colors: colors, track: track, loading: loading }}
-		>
-			<div
-				class={
-					styles.lfmvCard +
-					" glass relative mx-auto flex h-full w-full flex-col rounded-lg p-4 shadow-xl ring-2 ring-slate-950/5"
-				}
-				style={{ background: colors()?.primary }}
+		<>
+			{/* preconnects */}
+			<link href="https://lastfm.freetls.fastly.net" rel="preconnect" />
+			<link href="https://archive.org" rel="preconnect" />
+			<link href="https://coverartarchive.org" rel="preconnect" />
+			<link href="http://coverartarchive.org" rel="preconnect" />
+			<link href="https://musicbrainz.org" rel="preconnect" />
+			<link href="http://ws.audioscrobbler.com" rel="preconnect" />
+			{/* preconnects */}
+			<lfmContext.Provider
+				value={{ colors: colors, track: track, loading: loading }}
 			>
-				{track() instanceof Error ? (
-					<ErrorView message={message()} />
-				) : (
-					<>
-						<figure
-							class="mx-auto mb-2 h-auto overflow-hidden rounded-lg border-inherit"
-							style={{
-								"box-shadow": `0 0 20px ${colors()
-									?.secondary}99`
-							}}
-						>
-							{(track() as TrackInfo)?.lastfmImages &&
-							(
-								(track() as TrackInfo)
-									?.lastfmImages as LastFmImage[]
-							)[3]["#text"] ? (
-								<img
-									class="block h-full w-full overflow-hidden object-cover align-middle"
-									src={
-										(
-											(track() as TrackInfo)
-												?.lastfmImages as LastFmImage[]
-										)[3]["#text"]
-									}
-									alt="Album Cover"
-								/>
-							) : (track() as TrackInfo)?.MBImages ? (
-								<img
-									class="block h-full w-full overflow-hidden object-cover align-middle"
-									src={
-										(
-											(track() as TrackInfo)
-												?.MBImages as Image[]
-										)[0].image
-									}
-									alt="Album Cover"
-								/>
-							) : (
-								<img
-									src={disc}
-									class=""
-									alt="Default album cover thumbnail"
-								/>
-							)}
-						</figure>
-
-						<div class="flex h-min flex-col gap-1 drop-shadow-lg filter">
-							{(track() as TrackInfo)?.nowplaying ? (
-								<TrackProgressBar />
-							) : (
-								""
-							)}
-							<h1
-								class="shadow:lg mx-auto mt-1 text-center text-xs font-bold sm:text-base"
-								style={{ color: colors()?.secondary }}
+				<div
+					class={
+						styles.lfmvCard +
+						" glass relative mx-auto flex h-full w-full flex-col rounded-lg p-4 shadow-xl ring-2 ring-slate-950/5"
+					}
+					style={{ background: colors()?.primary }}
+				>
+					{track() instanceof Error ? (
+						<ErrorView message={message()} />
+					) : (
+						<>
+							<figure
+								class="mx-auto mb-2 h-auto overflow-hidden rounded-lg border-inherit"
+								style={{
+									"box-shadow": `0 0 20px ${colors()
+										?.secondary}99`
+								}}
 							>
-								{loading() ? (
-									<div class="skeleton h-8 w-fit"></div>
-								) : (track() as TrackInfo)?.trackName ? (
-									(track() as TrackInfo)?.trackName
+								{(track() as TrackInfo)?.lastfmImages &&
+								(
+									(track() as TrackInfo)
+										?.lastfmImages as LastFmImage[]
+								)[3]["#text"] ? (
+									<img
+										class="block h-full w-full overflow-hidden object-cover align-middle"
+										src={
+											(
+												(track() as TrackInfo)
+													?.lastfmImages as LastFmImage[]
+											)[3]["#text"]
+										}
+										alt="Album Cover"
+									/>
+								) : (track() as TrackInfo)?.MBImages ? (
+									<img
+										class="block h-full w-full overflow-hidden object-cover align-middle"
+										src={
+											(
+												(track() as TrackInfo)
+													?.MBImages as Image[]
+											)[0].image
+										}
+										alt="Album Cover"
+									/>
 								) : (
-									"Track title not available"
+									<img
+										src={disc}
+										class=""
+										alt="Default album cover thumbnail"
+									/>
 								)}
-							</h1>
-							<div
-								style={{ color: colors()?.secondary }}
-								class="flex flex-col gap-2 text-xs"
-							>
-								<LoadingSkeleton fallbackMsg="Artist name not available">
-									{
-										<span class="flex items-center justify-center gap-1">
-											<FaRegularUser />
-											{(track() as TrackInfo)?.artistName}
-										</span>
-									}
-								</LoadingSkeleton>
-								<LoadingSkeleton fallbackMsg="Album name not available">
-									{(track() as TrackInfo)?.albumTitle && (
-										<span class="flex items-center justify-center gap-1">
-											<FaSolidCompactDisc />
-											{(track() as TrackInfo)?.albumTitle}
-										</span>
-									)}
-								</LoadingSkeleton>
-							</div>
-							<PastTracks />
-							<div
-								style={{ color: colors()?.secondary }}
-								class="mt-2 flex  w-full justify-between drop-shadow-lg filter"
-							>
-								<span class="flex gap-2">
-									<a
-										href="https://www.last.fm/"
-										target="_blank"
-										class="h-min self-center "
-									>
-										<FaBrandsLastfm />
-									</a>
-									<a
-										href="https://musicbrainz.org/"
-										target="_blank"
-									>
-										<SiMusicbrainz />
-									</a>
-								</span>
-								<a
-									class=" flex items-center gap-2 text-xs"
-									href={`https://www.last.fm/user/${user}`}
+							</figure>
+
+							<div class="flex h-min flex-col gap-1 drop-shadow-lg filter">
+								{(track() as TrackInfo)?.nowplaying ? (
+									<TrackProgressBar />
+								) : (
+									""
+								)}
+								<h1
+									class="shadow:lg mx-auto mt-1 text-center text-xs font-bold sm:text-base"
+									style={{ color: colors()?.secondary }}
 								>
-									<FaRegularUser />
-									{user}
-								</a>
+									{loading() ? (
+										<div class="skeleton h-8 w-fit"></div>
+									) : (track() as TrackInfo)?.trackName ? (
+										(track() as TrackInfo)?.trackName
+									) : (
+										"Track title not available"
+									)}
+								</h1>
+								<div
+									style={{ color: colors()?.secondary }}
+									class="flex flex-col gap-2 text-xs"
+								>
+									<LoadingSkeleton fallbackMsg="Artist name not available">
+										{
+											<span class="flex items-center justify-center gap-1">
+												<FaRegularUser />
+												{
+													(track() as TrackInfo)
+														?.artistName
+												}
+											</span>
+										}
+									</LoadingSkeleton>
+									<LoadingSkeleton fallbackMsg="Album name not available">
+										{(track() as TrackInfo)?.albumTitle && (
+											<span class="flex items-center justify-center gap-1">
+												<FaSolidCompactDisc />
+												{
+													(track() as TrackInfo)
+														?.albumTitle
+												}
+											</span>
+										)}
+									</LoadingSkeleton>
+								</div>
+								<PastTracks />
+								<div
+									style={{ color: colors()?.secondary }}
+									class="mt-2 flex  w-full justify-between drop-shadow-lg filter"
+								>
+									<span class="flex gap-2">
+										<a
+											href="https://www.last.fm/"
+											target="_blank"
+											class="h-min self-center "
+										>
+											<FaBrandsLastfm />
+										</a>
+										<a
+											href="https://musicbrainz.org/"
+											target="_blank"
+										>
+											<SiMusicbrainz />
+										</a>
+									</span>
+									<a
+										class=" flex items-center gap-2 text-xs"
+										href={`https://www.last.fm/user/${user}`}
+									>
+										<FaRegularUser />
+										{user}
+									</a>
+								</div>
 							</div>
-						</div>
-					</>
-				)}
-			</div>
-		</lfmContext.Provider>
+						</>
+					)}
+				</div>
+			</lfmContext.Provider>
+		</>
 	);
 };
 

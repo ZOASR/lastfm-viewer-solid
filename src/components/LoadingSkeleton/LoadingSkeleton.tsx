aab14bf4d8c12b15a60895fc736f895cbee9e7b6
ZOASR <44146518+ZOASR@users.noreply.github.com>
2023-12-27 11:27:17 +0200

    refactor: refactored LoadingSkeleton component to use the new context

--- src/components/LoadingSkeleton/LoadingSkeleton.tsx
+++ src/components/LoadingSkeleton/LoadingSkeleton.tsx
@@ -1,5 +1,5 @@
 import { JSX } from "solid-js/jsx-runtime";
-import { useContext } from "solid-js";
+import { Show, useContext } from "solid-js";
 import { lfmContext } from "../SolidLastFMViewer";
 
 interface Props {
@@ -11,16 +11,19 @@ const LoadingSkeleton = (props: Props) => {
 	const context = useContext(lfmContext);
 	return (
 		<>
-			{(context.loading() as boolean) ? (
+			<Show
+				when={context.loading}
+				fallback={
+					<Show when={props.children} fallback={props.fallbackMsg}>
+						{props.children}
+					</Show>
+				}
+			>
 				<div class="flex justify-center">
 					<div class="skeleton mr-2 h-4 w-4 rounded-full"></div>
 					<div class="skeleton h-4 w-1/2"></div>
 				</div>
-			) : props.children ? (
-				props.children
-			) : (
-				<div>{props.fallbackMsg}</div>
-			)}
+			</Show>
 		</>
 	);
 };
